f48a4fd6fe7cf4093695112f47e989f3ea3a2c8f
Jyo-Patil <jyopatil221@gmail.com>
2025-08-25 16:44:40 +0530

    Code test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,20 @@
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
   }
-  required_version = ">= 1.0"
 }
-
 provider "aws" {
   region = var.aws_region
 }
